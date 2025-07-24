627f6a5b465c264c9447e3a6633f9eb63c197d72
RoundRobinHood <roundrobinhood@gmail.com>
2025-07-24 15:20:52 +0200

    Edge node typedefs

---
+++ backend/types/edge_nodes.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"context"
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type EdgeNode struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty"`
+	MAC    string             `bson:"mac"`
+	APIKey string             `bson:"apiKey"`
+}
+
+var ErrNodeNotExist = errors.New("Edge node does not exist")
+
+type EdgeNodeStore interface {
+	GetNodeByMAC(MAC string, ctx context.Context) (*EdgeNode, error)
+	GetNodeByID(ID primitive.ObjectID, ctx context.Context) (*EdgeNode, error)
+	CreateEdgeNode(node EdgeNode, ctx context.Context) (*EdgeNode, error)
+}
