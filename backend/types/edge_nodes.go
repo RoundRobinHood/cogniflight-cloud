bce298bd9cc2b46bf4f9db502db12a7dd85b59db
RoundRobinHood <roundrobinhood@gmail.com>
2025-07-25 21:17:59 +0200

    Implement DB store for API keys and update edge node type

--- backend/types/edge_nodes.go
+++ backend/types/edge_nodes.go
@@ -10,7 +10,6 @@ import (
 type EdgeNode struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty"`
 	MAC       string             `bson:"mac"`
-	APIKey    string             `bson:"apiKey"`
 	PlaneInfo struct {
 		TailNr string `bson:"tailNumber"`
 	} `bson:"planeInfo"`
@@ -21,6 +20,5 @@ var ErrNodeNotExist = errors.New("Edge node does not exist")
 type EdgeNodeStore interface {
 	GetNodeByMAC(MAC string, ctx context.Context) (*EdgeNode, error)
 	GetNodeByID(ID primitive.ObjectID, ctx context.Context) (*EdgeNode, error)
-	GetNodeByKey(APIKey string, ctx context.Context) (*EdgeNode, error)
 	CreateEdgeNode(node EdgeNode, ctx context.Context) (*EdgeNode, error)
 }
