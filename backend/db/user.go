f87839babef870d099a88f03512e7b1d580c19d6
RoundRobinHood <roundrobinhood@gmail.com>
2025-07-28 12:21:15 +0200

    Fix settings PATCH bugs

--- backend/db/user.go
+++ backend/db/user.go
@@ -7,6 +7,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type DBUserStore struct {
@@ -52,8 +53,62 @@ func (s DBUserStore) CreateUser(User types.User, ctx context.Context) (*types.Us
 }
 
 func (s DBUserStore) UpdateUser(ID primitive.ObjectID, update types.UserUpdate, ctx context.Context) (*types.User, error) {
+	set := bson.M{}
+	unset := bson.M{}
+	if update.Name != "" {
+		set["name"] = update.Name
+	}
+	if update.Email != "" {
+		set["email"] = update.Email
+	}
+	if update.Phone != "" {
+		set["phone"] = update.Phone
+	}
+	if update.PilotInfo.Provided {
+		if update.PilotInfo.Value == nil {
+			unset["pilotInfo"] = 1
+		} else {
+			beforeLen := len(set)
+			pilotInfo := *update.PilotInfo.Value
+			if pilotInfo.LicenseNr != "" {
+				set["pilotInfo.licenseNr"] = pilotInfo.LicenseNr
+			}
+			if pilotInfo.FlightHours.Provided {
+				set["pilotInfo.flightHours"] = pilotInfo.FlightHours.Value
+			}
+			if pilotInfo.Baseline.Provided {
+				if pilotInfo.Baseline.Value == nil {
+					unset["pilotInfo.baseline"] = 1
+				} else {
+					if len(pilotInfo.Baseline.Value) == 0 {
+						set["pilotInfo.baseline"] = bson.M{}
+					} else {
+						for key, val := range pilotInfo.Baseline.Value {
+							set["pilotInfo.baseline."+key] = val
+						}
+					}
+				}
+			}
+			if pilotInfo.EnvironmentPref.CabinTempPref.OptimalTemp.Provided {
+				set["pilotInfo.environmentPreferences.cabinTemperaturePreferences.optimalTemperature"] = pilotInfo.EnvironmentPref.CabinTempPref.OptimalTemp.Value
+			}
+			if pilotInfo.EnvironmentPref.CabinTempPref.ToleranceRange.Provided {
+				set["pilotInfo.environmentPreferences.cabinTemperaturePreferences.toleranceRange"] = pilotInfo.EnvironmentPref.CabinTempPref.ToleranceRange.Value
+			}
+			if pilotInfo.EnvironmentPref.NoiseSensitivity != "" {
+				set["pilotInfo.environmentPreferences.noiseSensitivity"] = pilotInfo.EnvironmentPref.NoiseSensitivity
+			}
+			if pilotInfo.EnvironmentPref.LightSensitivity != "" {
+				set["pilotInfo.environmentPreferences.lightSensitivity"] = pilotInfo.EnvironmentPref.LightSensitivity
+			}
+			if len(set) == beforeLen {
+				set["pilotInfo"] = bson.M{}
+			}
+		}
+	}
+
 	var updated types.User
-	if err := s.Col.FindOneAndUpdate(ctx, bson.M{"_id": ID}, bson.M{"$set": update}).Decode(&updated); err != nil {
+	if err := s.Col.FindOneAndUpdate(ctx, bson.M{"_id": ID}, bson.M{"$set": set, "$unset": unset}, options.FindOneAndUpdate().SetReturnDocument(options.After)).Decode(&updated); err != nil {
 		if err == mongo.ErrNoDocuments {
 			return nil, types.ErrUserNotExist
 		} else {
