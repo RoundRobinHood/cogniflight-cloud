6d10dd0eec2cf7829c0d0118e3dd286cd392b8c6
roundrobinhood <145403614+RoundRobinHood@users.noreply.github.com>
2025-09-07 14:30:27 +0200

    Add context types for OpenAI

--- backend/types/chatbot.go
+++ backend/types/chatbot.go
@@ -1,16 +1,27 @@
 package types
 
-type InputItemType string
+import "encoding/json"
+
+type ContextItemType string
 
 const (
-	Message            InputItemType = "message"
-	FunctionCall       InputItemType = "function_call"
-	FunctionCallOutput InputItemType = "function_call_output"
+	Message            ContextItemType = "message"
+	FunctionCall       ContextItemType = "function_call"
+	FunctionCallOutput ContextItemType = "function_call_output"
+	ReasoningMessage   ContextItemType = "reasoning"
 )
 
-type InputItem struct {
-	Type           InputItemType `json:"type"`
-	Role           string
-	CallID         string `json:"call_id"`
-	FunctionOutput any    `json:"output"`
+type ContextItem struct {
+	Type              ContextItemType `json:"type"`
+	Role              string          `json:"role,omitempty"`
+	CallID            string          `json:"call_id,omitempty"`
+	FunctionOutput    any             `json:"output,omitempty"`
+	FunctionArguments string          `json:"arguments,omitempty"`
+	Content           string          `json:"content,omitempty"`
+}
+
+type Function interface {
+	GetName() string                               // name of the function
+	Call(json.RawMessage) (json.RawMessage, error) // error for system-wide errors, json.RawMessage for result objects or validation errors
+	GetSchema() json.RawMessage                    // schema object
 }
