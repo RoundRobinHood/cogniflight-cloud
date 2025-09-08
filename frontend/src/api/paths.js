770b8905d72405ca5e800bb2366499853d01cee3
roundrobinhood <145403614+RoundRobinHood@users.noreply.github.com>
2025-09-08 21:42:09 +0200

    Add logout functions to api

--- frontend/src/api/paths.js
+++ frontend/src/api/paths.js
@@ -4,6 +4,7 @@ export default {
   base: API_PREFIX,
   whoami: API_PREFIX + '/whoami',
   login: API_PREFIX + '/login',
+  logout: API_PREFIX + '/logout',
 
   signup: {
     create_token: API_PREFIX + '/signup-tokens',
