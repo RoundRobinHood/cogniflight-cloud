936c6f44f9b3ff07cbd7eccdc1ae5a115f2cb6ab
roundrobinhood <145403614+RoundRobinHood@users.noreply.github.com>
2025-10-22 20:36:48 +0200

    Add edge-nodes command

--- backend/cmd/init_commands.go
+++ backend/cmd/init_commands.go
@@ -12,27 +12,33 @@ import (
 
 func InitCommands(filestore filesystem.Store, fsctx filesystem.FSContext, socketSession *types.SocketSession, sessionStore *types.SessionStore, apiKey chatbot.APIKey, jsonConn *jsonrpc2.Conn, mqttEvents *util.EventHandler[types.MQTTMessage], flux_cfg *influx.InfluxDBConfig) []sh.Command {
 	commands := []sh.Command{
+		&CmdWhoami{FileStore: filestore, Session: socketSession},
+		CmdHelp{},
+		&CmdChangePassword{FileStore: filestore},
+
+		CmdEcho{},
+		CmdError{},
+		CmdHeartbeat{},
+
 		&CmdLs{FileStore: filestore},
 		&CmdMkdir{FileStore: filestore},
 		&CmdCat{FSCtx: fsctx},
 		&CmdTee{FileStore: filestore},
 		&CmdRm{FileStore: filestore},
-		CmdEcho{},
-		CmdError{},
-		&CmdWhoami{FileStore: filestore, Session: socketSession},
-		&CmdClients{Socket: socketSession},
-		&CmdSockets{SessionStore: sessionStore},
-		&CmdPilots{FileStore: filestore},
 		&CmdMv{FileStore: filestore},
 		&CmdChmod{FileStore: filestore},
 		&CmdCopy{FileStore: filestore},
-		CmdHelp{},
-		CmdMLRPC{Conn: jsonConn},
+
 		&CmdEmbed{Conn: jsonConn, FileStore: filestore},
 		CmdCryptoRand{},
-		CmdHeartbeat{},
-		&CmdChangePassword{FileStore: filestore},
 
+		&CmdClients{Socket: socketSession},
+		&CmdSockets{SessionStore: sessionStore},
+
+		&CmdPilots{FileStore: filestore},
+		&CmdEdgeNodes{FileStore: filestore},
+
+		CmdMLRPC{Conn: jsonConn},
 		CmdFluxStream{FluxCfg: flux_cfg},
 		&CmdMQTT{Events: mqttEvents},
 
